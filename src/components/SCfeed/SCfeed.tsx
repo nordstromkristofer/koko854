cf1f6f78bb2878b720d4f0b3789daa352df97301
nordstromkristofer <nordstromkristofer@gmail.com>
2023-05-07 23:42:07 +0200

    b4 heroku fix

--- src/components/SCfeed/SCfeed.tsx
+++ src/components/SCfeed/SCfeed.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 import ReactPlayer from "react-player"
 
+import './SCfeed.css'
+
 function SCfeed() {
   return (
-    <div>
-      <ReactPlayer
+    <div >
+      <ReactPlayer className="SCplayer"
         url="https://soundcloud.com/kristougha/pressurin"
       />
     </div>
