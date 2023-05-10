bdcc0afe79f1eae44ddfe09c887dd70ac62e24ae
nordstromkristofer <nordstromkristofer@gmail.com>
2023-05-10 12:08:48 +0200

    multiple tracks

--- src/components/SCfeed/SCfeed.tsx
+++ src/components/SCfeed/SCfeed.tsx
@@ -4,11 +4,23 @@ import ReactPlayer from "react-player"
 import './SCfeed.css'
 
 function SCfeed() {
+  const tracks = [
+    "https://soundcloud.com/kristougha/radio854vol1",
+    "https://soundcloud.com/kristougha/pressurin",
+    "https://soundcloud.com/gull33/koko-toyan-gull33-guest-mix-01",
+    "https://soundcloud.com/kristougha/kokoivan-mix-v1",
+    "https://soundcloud.com/kristougha/radio854vol2"
+  ];
+
   return (
-    <div >
-      <ReactPlayer className="SCplayer"
-        url="https://soundcloud.com/kristougha/pressurin"
-      />
+    <div>
+      {tracks.map((track, index) => (
+        <ReactPlayer
+          key={index}
+          className="SCplayer"
+          url={track}
+        />
+      ))}
     </div>
   )
 }
