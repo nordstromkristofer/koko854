fa7d4717aa919ca63d5dfc195a5de6991e6b6075
nordstromkristofer <nordstromkristofer@gmail.com>
2023-05-10 15:37:54 +0200

    soundcloud feed styling

--- src/components/SCfeed/SCfeed.tsx
+++ src/components/SCfeed/SCfeed.tsx
@@ -1,27 +1,81 @@
-import React from "react"
+import React, { useState } from "react"
 import ReactPlayer from "react-player"
+import Card from '@mui/material/Card';
+
 
 import './SCfeed.css'
 
 function SCfeed() {
   const tracks = [
     "https://soundcloud.com/kristougha/radio854vol1",
+    "https://soundcloud.com/kristougha/radio854vol2",
     "https://soundcloud.com/kristougha/pressurin",
     "https://soundcloud.com/gull33/koko-toyan-gull33-guest-mix-01",
     "https://soundcloud.com/kristougha/kokoivan-mix-v1",
-    "https://soundcloud.com/kristougha/radio854vol2"
+    "https://soundcloud.com/1992presents/1992-presents-koko854-20"
   ];
+
+  const [playingUrl, setPlayingUrl] = useState<string | null>(null);
+  const [playing, setPlaying] = useState<boolean>(false);
+
+  const handlePlay = (url: string) => {
+    console.log("handlePlay called");
+    setPlayingUrl(url);
+    setPlaying(true);
+  };
+
+
+  const handlePause = () => {
+    console.log("handlePause");
+    setPlaying(false);
+  };
+
+  const playerStyle = {
+    borderRadius: '20px',
+    backgroundColor: '#e0e0e0',
+    overflow: 'hidden'
+  }
+
+  const playerConfig = {
+    soundcloud: {
+      options: {
+        show_artwork: true,
+        show_comments: false,
+        show_user: true,
+        visual: true,
+        color: "#cb00ff"
+      }
+    }
+  }
+
+  const cardStyle = {
+    borderRadius: '20px',
+    overflow: 'hidden'
+  };
+
+  console.log("render");
 
   return (
     <div>
-      {tracks.map((track, index) => (
-        <ReactPlayer
-          key={index}
-          className="SCplayer"
-          url={track}
-        />
-      ))}
+
+      <div className="SCfeed">
+        {tracks.map((track, index) => (
+          <div key={index} className="SCtrack" onClick={() => handlePlay(track)}>
+            <Card className="SoundCard" sx={{ width: "100%", display: "flex" }} style={cardStyle}>
+              <ReactPlayer
+                className="SCplayer"
+                url={track}
+                width="440px"
+                height="160px"
+                style={playerStyle}
+                config={playerConfig}
+              />
+            </Card>
+          </div>
+        ))}
+      </div>
     </div>
+
   )
 }
 
