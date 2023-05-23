880abafa0d8bf709b3d61697e5b21a89da0892f1
nordstromkristofer <nordstromkristofer@gmail.com>
2023-05-23 15:11:06 +0200

    headerosv fix

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import './header.css'
+import { FiHome } from 'react-icons/fi'
 
 const Header: React.FC = () => {
   return (
@@ -8,7 +9,13 @@ const Header: React.FC = () => {
       <header>
         <nav>
           <div className="links">
-            <Link to="/" className="linktext home">Home</Link>
+            <Link to="/" className="homesymbollink">
+              <FiHome className="homeSymbol" size="35px" color="white" margin-top="10px" />
+            </Link>
+            <Link to="/" className="linktext linktext-home">
+              Home
+            </Link>
+
             <Link to="/instafeed" className="linktext">Instafeed</Link>
             <Link to="/soundcloud" className="linktext">Soundcloud</Link>
           </div>
